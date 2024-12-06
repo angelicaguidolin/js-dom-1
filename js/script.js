eefbc17fb7164eee6a9a26c09813e1a78eea6cf8
angelica <guidolinangelica66@gmail.com>
2024-12-06 16:42:39 +0100

    feat add on

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// JavaScript code
+const photoElement = document.getElementById("on-off");
+const buttonElement = document.getElementById("btn-on-off");
+
+buttonElement.addEventListener("click", function() {
+  photoElement.src = "./img/yellow_lamp.png";
+});
+   
